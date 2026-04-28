d3c0eac3f5a181102fef6e187c7940dfe9fd52a6
xannnimal <amcphers@stanford.edu>
2026-04-28 16:28:11 -0700

    add basic events, epochs, evoked steps

--- MovementPilot_Processing.py
+++ MovementPilot_Processing.py
@@ -15,9 +15,44 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import mne
+import pandas as pd
 
 
 # ------ funcs ----------------------------------------------------------------
+def get_events_fif(raw,file):
+    ## TODO: go through evends, made pandas df
+    event_code_list = events[:, 2]
+    event_code_updates = np.zeros_like(event_code_list)
+    special_codes = np.array([63,110])
+    # ei = 0
+    # while ei < len(event_code_list):
+    #     event = event_code_list[ei]
+    #     if event in special_codes:
+    #         event_code_updates[ei+1:ei+5] = event
+    #         event_code_updates[ei] = 201  # code for what was the condition label
+    #         ei += 4  # skip next 4 positions 
+    #     else:
+    #         ei += 1  # just advance by 1 if no match
+    # events[:, 2] = event_code_updates
+    # # get just the code part
+    # trigger_codes = events[:, 2]
+    # blocks = trigger_codes.reshape(-1, 5)
+    # blocks_rearranged = blocks[:, [1, 2, 3, 4, 0]]
+    # result = blocks_rearranged.flatten()
+    # events[:, 2] = result
+    ## make event codes interpretable
+    code_dict = {63: "code1",
+                 110: "code2",
+                 255: "code3"}
+    
+    # # make into a nice pandas dataframe
+    # events_df = pd.DataFrame()
+    # events_df['code'] = events[:, 2]
+    # events_df['condition'] = [code_dict[c].split('_')[0] for c in events[:, 2]]
+    # events_df['font'] = [code_dict[c].split('_')[1] for c in events[:, 2]]
+    
+    return
+
 def highpass_filter_opm(raw):
     freq_min = 1
     freq_max = 70
@@ -35,26 +70,6 @@ def ssp_filter(raw):
 def sss_prepros(raw):
     raw_sss = mne.preprocessing.maxwell_filter(raw, origin=(0., 0., 0.), int_order=8, ext_order=3, calibration=None, coord_frame='meg', regularize=None, ignore_ref=True, bad_condition='ignore', mag_scale=100.0, extended_proj=(), verbose=None)
     return raw_sss
-
-# mSSS ------------------------------------------------------------------------
-def _combine_sss_basis(S_in1, S_in2):
-    S_tot = [];
-    thresh = 5e-7 #0.005 in Matlab, we will need to do some further investigating here once your code works
-    U, s, Vh = np.linalg.svd(np.concatenate((S_in1,S_in2),axis=1))
-    #apply threshold to limit dimensions of resulting basis
-    for i in range(0, np.shape(s)[0]):
-        ratio = s[i]/s[0]
-        if ratio >= thresh:
-            S_tot.append(U[:,i])
-    return np.transpose(np.array(S_tot))
-
-
-def mSSS_recon(raw, S_tot):
-    phi_0 = raw.get_data(picks='meg')
-    pS = np.linalg.pinv(S_tot)
-    XN = pS @ phi_0
-    data_mSSS = np.real(S_tot@XN)
-    return data_mSSS
 
 ## Fosters --------------------------------------------------------------------
 def _do_inverse(raw,N):
@@ -134,74 +149,6 @@ def fosters_inverse(raw):
     
     return raw_fos
 
-def fosters_inverse_msss(raw,origins):
-    """
-    Parameters
-    ----------
-    raw : mne.raw structure
-        full raw meg file, ex. "fif", from recording with raw.info["bads"] indicated
-    origins: list of two 1x3 np.array
-        two origins optimized using "fit_sphere_mri.py" to fit the internal brain
-        without encoraching on sensors
-    
-    Returns
-    -------
-    raw_fos : mne.raw structure
-        raw strucutre with the MEG data updated with the Fosters Inverse 
-        preprocessed data, raw.info structure updated to indicate some type of
-        Maxwell Filtering/SSS preprocessing has occured. Channels marked "bad" 
-        are dropped
-    """
-    ## calculate sensor noise covariance
-    N = mne.compute_raw_covariance(raw,rank="info",method='empirical')["data"]
-    ## create data strcutre, indicates in "info" that some preprocessing akin to SSS has happened
-    raw_fos_msss = mne.preprocessing.maxwell_filter(raw, origin=(0.,0.,0.), int_order=8, ext_order=3, calibration=None, coord_frame='meg', regularize='in', ignore_ref=True, bad_condition='error', mag_scale=100.0, extended_proj=(), verbose=None)  # just to get the info to indicate some Maxwell filtering was done etc.
-    #assert raw.info["bads"] == [] # double check bads were dropped
-    ## extract raw data matrix from MEG channels
-    phi_0 = raw_fos_msss.get_data(picks='meg')
-    
-    ## calculate mSSS matrix 
-    [S_1, f, reg_moments, n_use_in1]=mne.preprocessing.compute_maxwell_basis(raw.info, origin=origins[0], int_order=8, ext_order=3, calibration=None, coord_frame='meg', regularize=None, ignore_ref=True, bad_condition='ignore', mag_scale=100.0, extended_proj=(), verbose=None)
-    [S_2, f, reg_moments, n_use_in2]=mne.preprocessing.compute_maxwell_basis(raw.info, origin=origins[1], int_order=8, ext_order=3, calibration=None, coord_frame='meg', regularize=None, ignore_ref=True, bad_condition='ignore', mag_scale=100.0, extended_proj=(), verbose=None)
-    S_tot= _combine_sss_basis(S_1[:, :n_use_in1], S_2[:, :n_use_in2])
-    
-    [S, f, reg_moments, n_use_in]=mne.preprocessing.compute_maxwell_basis(raw.info, origin=[0,0,0], int_order=8, ext_order=3, calibration=None, coord_frame='meg', regularize=None, ignore_ref=True, bad_condition='error', mag_scale=100.0, extended_proj=(), verbose=None)
-    ## setup Foster's Inverse- calculate Matrix B and vector b
-    # #for full basis
-    S_out = S[:, n_use_in:]
-    S=np.concat((S_tot,S_out),axis=1)
-    # #for internal only
-    # S=S_tot
-    pS = np.linalg.pinv(S)
-    XN = pS @ phi_0
-    
-    alpha = np.transpose(XN)
-    alpha_cov_norm = np.cov(XN)
-    S_star = np.transpose(np.conj(S))
-    first = np.linalg.pinv(S@alpha_cov_norm@S_star +N)
-    B = alpha_cov_norm @ S_star @ first
-    m_alpha = np.transpose(np.mean(alpha,0))
-    b = m_alpha - B@S@m_alpha
-    x_bar = np.zeros_like(XN)
-    
-    ## calculate Foster's Inverse estimate of multipole moments
-    for i in range(0,np.shape(phi_0)[1]):
-        x_bar[:,i]=B@phi_0[:,i] + b
-    
-    ## use new estimate to reconstruct internal data
-    data_fosters = np.real(S_tot@x_bar[:np.size(S_tot,1),:])
-    
-    ## isolate MEG channels 
-    meg_picks = mne.pick_types(raw.info, meg=True)
-    ## put new Foster's inverse recon data into "raw" structure
-    raw_fos_msss._data[meg_picks] = data_fosters
-    
-    ## cleanup
-    del data_fosters
-    
-    return raw_fos_msss
-
-
 #### calculate SNR
 def calculate_snr(signal, noise):
     signal_power = np.mean(np.var(signal))
@@ -227,7 +174,7 @@ if __name__ == '__main__':
 
     
     ##-- define constants -----------------------------------------------
-    compare_prepross=False
+    compare_prepross=True
     
     n_trials = len(sample_files)
     trigger_chan = 'di2'  
@@ -246,8 +193,8 @@ if __name__ == '__main__':
     )
     baseline=None
     
-    freq_min = None
-    freq_max = 100
+    freq_min = 0.5
+    freq_max = 80
     
     ## -- Look at data + try some preprocessing to clean it -------------------
     raws =[]
@@ -266,19 +213,43 @@ if __name__ == '__main__':
                 bads_list.append(raw.info["chs"][i]["ch_name"])
         raw.drop_channels(bads_list)
         bads = raw.info["bads"]
-        #drop bad channels
         raw.drop_channels(bads)
 
         ## high and low - pass, notch filter raw data--------------------------
         raw.load_data().filter(l_freq=freq_min, h_freq=None)
         raw.load_data().filter(l_freq=None, h_freq=freq_max)
         meg_picks = mne.pick_types(raw.info, meg=True)
-        #raw.notch_filter(freqs=60, picks=meg_picks)
+        raw.notch_filter(freqs=60, picks=meg_picks)
         raws.append(raw)
+        fig = raw.compute_psd(fmax=freq_max).plot(average=False, amplitude=False, picks="data", exclude="bads")
+
+        ## -- get events ---------------------------------------------------
+        ## TODO: parse events
+        ## [events_df,events,task] = get_events_fif(raw,file)
+        events = mne.find_events(raw, stim_channel=trigger_chan, shortest_event=1)
+        info = raw.info
+        picks = 'mag'
+        reject_criteria = dict(mag=4000e-15)
         
-        #fig = raw.compute_psd(fmax=freq_max).plot(average=False, amplitude=False, picks="data", exclude="bads")
-
-
+        # --- 3. Make Epochs and Evokeds --------------------------------------
+        ## very basic example -- we will do something fancier
+        tmin = -0.05  # start of each epoch (200ms before the trigger)
+        tmax = 0.3  # end of each epoch (600ms after the trigger)
+        baseline = None
+        # can add rejection criteria based on P-to-P signal        
+        # separate out by event ID
+        epochs = mne.Epochs(raw, events, 
+                    tmin=tmin, tmax=tmax,
+                    baseline=baseline,
+                    reject=None,
+                    preload=True)
+        evoked = epochs.average()
+        ## specify plotting args
+        ts_args = ts_args = dict(time_unit="s") 
+        topomap_args = dict(time_unit="s") 
+        fig = evoked.plot_joint(times="peaks", ts_args=ts_args, topomap_args=topomap_args, title= subject+ ' Task: '+ task)
+        
+     
         
         if compare_prepross:
             ## SSP ----------------------------------------------------------------
@@ -292,11 +263,23 @@ if __name__ == '__main__':
             raw_fos = fosters_inverse(raw)
             
             ## joint plot compare -------------------------------------------------            
-            fig, axes = plt.subplots(4, 1, sharey=True, layout="constrained", figsize=(10, 10))
-            for ax, data, title in zip(axes, [raw, raw_ssp, raw_sss,raw_fos], ["Raw Empty Room Data", "SSP Preprocessed","SSS Preprocessed", "Fosters Inverse Preprocessed"]):
-                fig = data.compute_psd(fmax=freq_max).plot(average=True, amplitude=False, picks="data", exclude="bads", axes=ax)
-                ax.set(title=subject+', '+task+', '+title, xlim=(0, freq_max), ylim=(0,90))
+            # fig, axes = plt.subplots(4, 1, sharey=True, layout="constrained", figsize=(10, 10))
+            # for ax, data, title in zip(axes, [raw, raw_ssp, raw_sss,raw_fos], ["Raw Empty Room Data", "SSP Preprocessed","SSS Preprocessed", "Fosters Inverse Preprocessed"]):
+            #     fig = data.compute_psd(fmax=freq_max).plot(average=True, amplitude=False, picks="data", exclude="bads", axes=ax)
+            #     ax.set(title=subject+', '+task+', '+title, xlim=(0, freq_max), ylim=(0,90))
+            for raw,method in zip([raw_ssp,raw_sss,raw_fos],["SSP","SSS","Fosters"]):
+                epochs = mne.Epochs(raw, events, 
+                            tmin=tmin, tmax=tmax,
+                            baseline=baseline,
+                            reject=None,
+                            preload=True)
+                evoked = epochs.average()
+                ## specify plotting args
+                ts_args = ts_args = dict(time_unit="s") 
+                topomap_args = dict(time_unit="s") 
+                fig = evoked.plot_joint(times="peaks", ts_args=ts_args, topomap_args=topomap_args, title= subject+ ' '+ method +' Task: '+ task)
                 
+
 ## just look at raws for each movement            
 fig, axes = plt.subplots(n_trials, 1, sharey=True, layout="constrained", figsize=(10, 20))
 for ax, raw, task in zip(axes, raws, tasks):
